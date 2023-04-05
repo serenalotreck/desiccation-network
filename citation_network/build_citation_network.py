6b082ae8a1628df1f2bf172c578f4debdf5c54d7
SLotreck <lotrecks@msu.edu>
2023-04-05 18:13:51 -0400

    Add TaxoNERD classification to citation network and debug

--- citation_network/build_citation_network.py
+++ citation_network/build_citation_network.py
@@ -6,18 +6,98 @@ Author: Serena G. Lotreck
 import argparse
 from os.path import abspath
 from tethne.readers import wos
+from tqdm import tqdm
+from tethne.classes.corpus import Corpus
 from tethne.networks.papers import direct_citation
 from tethne.writers.graph import to_graphml
 from taxonerd import TaxoNERD
+import taxoniq
 
 
-def classify_paper(paper, nlp):
+def classify_orgs(ents, defs):
+    """
+    Get organism classifications from a list of NCBI Taxonomy IDs
+
+    parameters:
+        ents, list of int: NCBI Taxonomy ID's
+        defs, dict: keys are lineage categories, values are the final
+            kingdom classification for those categories
+
+    returns:
+        kings, list of str: unique kingdom classifications
+    """
+    kings = []
+    for i in ents:
+        t1 = taxoniq.Taxon(i)
+        lineage = [t.scientific_name for t in t1.ranked_lineage]
+        if lineage[-1] == 'Bacteria' or lineage[-1] == 'Archea':
+            kings.append(defs[lineage[-1]])
+        elif lineage[-1] == 'Eukaryota':
+            try:
+                kings.append(defs[lineage[-2]])
+            except KeyError:
+                continue
+
+    kings = list(set(kings))
+    return kings
+
+
+def get_kingdom(title, abstract, taxonerd):
+    """
+    Uses TaxoNERD to get ID's from title and abstract, gets kingdoms and
+    compares to get the final kingdom classification for the article.
+
+    parameters:
+        title, str: title of the article
+        abstract, str: abstract of the article
+        taxonerd, TaxoNERD instance: TaxoNERD model
+
+    returns:
+        king, str: kingdom for the study organism of the paper
+    """
+    # Do TaxoNERD classification
+    title_df = taxonerd.find_in_text(title)
+    abstract_df = taxonerd.find_in_text(abstract)
+
+    # Get the unique organisms
+    title_ents = list(set([title_df['entity'][j][0][0].split("NCBI:")[1] for j in
+            range(len(title_df))]))
+    abstract_ents = list(set([abstract_df['entity'][j][0][0].split("NCBI:")[1] for j in
+            range(len(abstract_df))]))
+
+    # Set up definitions for kingdom classification
+    defs = {
+            'Metazoa': 'Animal',
+            'Viridiplantae': 'Plant', # Consider adding algae
+            'Bacteria': 'Microbe',
+            'Archea': 'Microbe'
+            }
+
+    # Classify unique organisms
+    title_classes = classify_orgs(title_ents, defs)
+    abstract_classes = classify_orgs(abstract_ents, defs)
+
+    # Compare title and abstract
+    if (len(title_classes) == len(abstract_classes) == 1) and (title_classes ==
+            abstract_classes):
+        king = title_classes[0]
+        return king
+    elif len(title_classes) == 0 and len(abstract_classes) == 1:
+        return abstract_classes[0]
+    elif len(abstract_classes) == 0 and len(title_classes) == 1:
+        return title_classes[0]
+    else:
+        # For now, come back to this
+        return None
+
+
+def classify_paper(paper, taxonerd):
     """
     Get the study organism kingdom from a paper.
 
     parameters:
         paper, tethne.classes.paper.Paper object: paper to check
-        nlp, spacy NLP object: TaxoNERD spacy pipeline
+        taxonerd, TaxoNERD instance: TaxoNERD model
 
     returns:
         king, str: 'Animal', 'Plant' or 'Microbe' if paper contains at least
@@ -33,21 +113,15 @@ def classify_paper(paper, nlp):
     try:
         abstract = paper['abstract']
     except AttributeError:
-        abstarct = ''
+        abstract = ''
 
-    # If one is missing, operate only on the other, if both gone, ignore
-    if title == '' and abstract != '':
-        pass
-    elif (title != '' and abstract == ''):
-        pass
-    elif title == '' and abstract == '':
-        return None
-    else:
-        # Do the thing
-        pass
+    # Get the kingdom
+    king = get_kingdom(title, abstract, taxonerd)
+
+    return king
 
 
-def classify_nodes(papers, nlp, out_loc, out_prefix):
+def classify_nodes(papers, taxonerd, out_loc, out_prefix):
     """
     Find the kingdom (plant, animal, microbe) of the study organism of each
     paper. Follows this general algorithm:
@@ -68,43 +142,79 @@ def classify_nodes(papers, nlp, out_loc, out_prefix):
 
     paramters:
         papers, tethne.classes.Corpus object: papers to classify
-        nlp, spacy NLP object: TaxoNERD spacy pipeline
+        taxonerd, TaxoNERD instance: TaxoNERD model
         out_loc, str: directory to save output
         out_prefix, str: string to prepend to output files
 
     returns:
         TODO decide what output format is best
     """
-    for paper in papers:
+    dropped_papers = []
+    keep_papers = []
+    paper_kings = {}
+    for paper in tqdm(papers):
 
         # Get kingdom
-            king = classify_paper(paper, nlp)
+        king = classify_paper(paper, taxonerd)
 
-            if king is None:
-                # Do something to save to missed outputs
-                pass
-            else:
-                # Do something to put in what gets returned
-                pass
+        if king is None:
+            # Save to dropped outputs
+            dropped_papers.append(paper)
+        else:
+            try:
+                # Save the taxonomic classification
+                paper_kings[paper.doi] = king
+                # Save kept papers
+                keep_papers.append(paper)
+            except AttributeError:
+                # Drop if it doesn't have a DOI
+                dropped_papers.append(paper)
 
+    # Make and save new corpus of dropped papers
+    drop_corpus = Corpus(dropped_papers)
+    drop_save = f'{out_loc}/{out_prefix}_classification_dropped_documents.hdf5'
+    _ = drop_corpus.to_hdf5(datapath=drop_save)
+    verboseprint('Papers that could not be classified by study organism have '
+                f'been saved to {drop_save}. {len(dropped_papers)} were dropped.')
+
+    # Generate a summary of classification
+    counts = Counter(paper_kings.values())
+    verboseprint('A summary of the identified classes:')
+    verboseprint(counts)
+
+    # Make new corpus with kept papers and add taxonomic class as feature
+    keep_corpus = Corpus(keep_papers, index_by='doi')
+    keep_corpus.add_features('kingdom_class', paper_kings)
+    keep_save = f'{out_loc}/{out_prefix}_classification_kept_documents.hdf5'
+    _ = keep_corpus.to_hdf5(datapath=keep_save)
+    verboseprint('Papers with successfull study organism classification have '
+            f'been saved to {keep_save}. {len(keep_papers)} were kept.')
+    return keep_corpus
 
 
 def main(wos_file, out_loc, out_prefix):
 
     # Read in the WOS file
+    verboseprint('\nReading in WOS output...')
     papers = wos.read(wos_file)
 
     # Classify the nodes by study system
-    taxonerd = TaxoNERD(prefer_gpu=True)
+    verboseprint('\nLoading TaxoNERD model...')
+    taxonerd = TaxoNERD(prefer_gpu=False)
     nlp = taxonerd.load(model="en_core_eco_biobert", linker="ncbi_taxonomy", threshold=0.7)
-    classed_papers = classify_nodes(papers, nlp)
+    verboseprint('\nClassifying papers by study organism...')
+    classed_papers = classify_nodes(papers, taxonerd, out_loc, out_prefix)
 
     # Build the network
-    graph = direct_citation(papers)
+    verboseprint('\nBuilding citation network...')
+    graph = direct_citation(classed_papers)
 
     # Output in file renderable by cytoscape
+    verboseprint('\nSaving citation network...')
     out_file = f'{out_loc}/{out_prefix}_citation_network.graphml'
     to_graphml(graph, out_file)
+
+    verboseprint('\nDone!\n')
 
 if __name__ == "__main__":
 
